42e470beb2f3b95b25b2ccf5f3cad89c75c907b2
Gaurav Mori <gkmori444@gmail.com>
2023-02-03 18:24:29 +0530

    practice

---
+++ 4. Typescript/generics.js
@@ -0,0 +1,7 @@
+var makeArr = function (x, y) {
+    return [x, y];
+};
+var v = makeArr("hello", 3);
+var v1 = makeArr(1, 1);
+console.log(v1);
+console.log(v);
---
+++ 4. Typescript/generics.ts
@@ -0,0 +1,9 @@
+
+const makeArr =<T,Y> (x:T ,y:Y) =>{
+return [x,y];
+}
+
+let v= makeArr("hello",3);
+let v1 =makeArr(1,1);
+console.log(v1)
+console.log(v);
